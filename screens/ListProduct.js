72ba9d6bf2531bd47f857210df314be678c76908
ind-agr <in.agramonte@gmail.com>
2023-10-03 23:43:35 -0500

    Lista, mostrar e eliminar

--- screens/ListProduct.js
+++ screens/ListProduct.js
@@ -1,7 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import {View, Text, TouchableOpacity, ScrollView, StyleSheet} from 'react-native'
+
+import appFirebase from '../credenciales'
+import {getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoct} from 'firebase/firestore'
+const db = getFirestore(appFirebase)
 
 export default function ListProduct(props) {
+    const [lista, setLista] = useState([])
+
+
+    // logica para llamara la lista de documentos de la coleccion 
+  useEffect(() => {
+    const getLista = async()=>{
+        try {
+            const querySnapshot = await getDocs(collection(db, 'producto'))
+            const docs = []
+            querySnapshot.forEach((doc)=>{
+                const {nombre, color, stock} = doc.data()
+                docs.push({
+                    id:doc.id,
+                    nombre,
+                    color,
+                    stock,
+                })
+            })
+            setLista(docs);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getLista()
+}, [lista])
+
   return (
     <ScrollView>
       <TouchableOpacity style={styles.Boton} onPress={()=>props.navigation.navigate('Create')}>
@@ -11,13 +41,26 @@ export default function ListProduct(props) {
       <View>
         <Text style = {styles.TextoTitulo}>Lista de los Productos</Text>
       </View>
+
+      <View>
+        {
+          lista.map((list)=>(
+            <TouchableOpacity key={list.id} style={styles.BotonLista} 
+            onPress={()=>props.navigation.navigate('Show',{productoId:list.id})}>
+                <Text style={styles.TextoNombre}>-{list.nombre}</Text>
+            </TouchableOpacity>
+          ))
+        }
+      </View>
+
+
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
     Boton:{
-        backgroundColor:'cyan',
+        backgroundColor:'#007BFF',
          height:35,
          borderColor:'black',
         borderWidth:1     
--- screens/ShowProduct.js
+++ screens/ShowProduct.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import {View, Text, TouchableOpacity, ScrollView, StyleSheet, Alert} from 'react-native'
 
-export default function ShowProduct() {
+import appFirebase from '../credenciales'
+import {getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoct} from 'firebase/firestore'
+
+const db = getFirestore(appFirebase)
+
+export default function ShowProduct(props) {
+
+  const [product, setProduct] = useState({})
+
+  const getOneProduct = async(id)=>{
+    try{
+      const docRef = doc(db, 'producto', id)
+      const docSnap = await getDoc(docRef)
+      setProduct(docSnap.data())
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getOneProduct(props.route.params.productoId)
+  },[])
+
+  const deleteProduct = async(id)=>{ 
+    Alert.alert('exito', 'producto eliminado con exito')
+    props.navigation.navigate('List')
+  }
+
+
   return (
-    <ScrollView>
-      <TouchableOpacity>
-        <Text>Agregar Productos</Text>
+    <View>
+      <Text style={styles.titulo} >Detalle del producto</Text>
+      
+        <Text style={styles.sub}>Nombre: {product.nombre}</Text>
+        <Text style={styles.sub}>Color: {product.color}</Text>
+        <Text style={styles.sub}>Precio: {product.stock}</Text>
+      
+      <TouchableOpacity style={styles.BotonLista} onPress={()=>deleteProduct(props.route.params.productoId)}>
+         <Text style={styles.TextoNombre}>Eliminar</Text>
       </TouchableOpacity>
-    </ScrollView>
-  );
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  titulo:{
+    textAlign:'center',
+    marginTop:10,
+    marginBottom:10,
+    fontSize:20
   },
-});
+  sub:{
+    fontSize:16
+  },
+  
+  TextoNombre:{
+    fontSize:16,
+    textAlign:'center',
+    color:'white',
+    
+  },
+  BotonLista:{
+    backgroundColor:'red',
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc',
+    marginBottom:3,
+    padding:5,
+    marginTop:5
+  }
+})
